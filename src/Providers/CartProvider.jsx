329c81eba107bde21a1406d477923d8d568bcdf7
MD. Mehedi Hasan talha <mdmehedihasanbd2006@gmail.com>
2024-11-06 22:50:30 +0600

    after resolve

--- src/Providers/CartProvider.jsx
+++ src/Providers/CartProvider.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { CartContext } from "../Contexts";
 import showToastMessage from "../utils/showToastMessage";
-import textShortener from "../utils/textShortener";
+// import textShortener from "../utils/textShortener";
 
 export default function CartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
@@ -29,4 +29,12 @@ export default function CartProvider({ children }) {
       {children}
     </CartContext.Provider>
   );
+}
+
+function textShortener(text, maxChar) {
+  if (text.length > maxChar) {
+    return `${text.slice(0, maxChar)}...`;
+  }
+
+  return text;
 }
--- src/utils/textShortener.js
+++
@@ -1,7 +0,0 @@
-export default function textShortener(text, maxChar) {
-  if (text.length > maxChar) {
-    return `${text.slice(0, maxChar)}...`;
-  }
-
-  return text;
-}
